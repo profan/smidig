2811b5c3d7e15ccf777193f185032f7d4ecc3192
Robin Hübner <profan@pagefault.se>
2015-02-22 16:46:51 +0100

    Make sure to keep around reference to string in D code.

--- source/window.d
+++ source/window.d
@@ -1,19 +1,21 @@
 module sundownstandoff.window;
 
+import std.utf : toUTFz;
 import std.stdio : writefln;
-import std.string : toStringz;
 
 import derelict.sdl2.sdl;
 
 struct Window {
 
 	bool alive;
+	char* c_title; //keep this here so the char* for toStringz doesn't point to nowhere!
 	SDL_Window* window;
 	SDL_Renderer* renderer;
 
 	this(in char[] title, uint width, uint height) {
+		this.c_title = toUTFz!(char*)(title);
 		this.window = SDL_CreateWindow(
-			toStringz(title), 
+			c_title,
 			SDL_WINDOWPOS_UNDEFINED,
 			SDL_WINDOWPOS_UNDEFINED,
 			width, height,
@@ -33,6 +35,9 @@ struct Window {
 		SDL_DestroyWindow(window);
 
 	}
+
+	@property const(char*) title() { return c_title; }
+	@trusted @property void title(in char[] new_title) { c_title = toUTFz!(char*)(new_title); SDL_SetWindowTitle(window, c_title); }
 
 	void render_clear() {
 		SDL_RenderClear(renderer);
