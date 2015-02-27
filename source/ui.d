a751e2081c19359fff929010b5bc632a10e76ca5
Robin Hübner <profan@pagefault.se>
2015-02-27 23:34:03 +0100

    Added push/pop color set.

--- source/ui.d
+++ source/ui.d
@@ -4,19 +4,28 @@ import derelict.sdl2.sdl;
 
 import sundownstandoff.window;
 
+struct PushColor {
+
+	ubyte r, g, b, a;
+	Window* window;
+
+	this(Window* window, ubyte r, ubyte g, ubyte b, ubyte a) {
+		this.window = window;
+		SDL_GetRenderDrawColor(window.renderer, &this.r, &this.g, &this.b, &this.a);
+		SDL_SetRenderDrawColor(window.renderer, r, g, b, a);
+	}
+
+	~this() {
+		SDL_SetRenderDrawColor(window.renderer, r, g, b, a);
+	}
+
+} //DrawColor
+
 //Immediate Mode GUI (IMGUI, see Muratori)
 void draw_rectangle(Window* window, bool filled, int x, int y, int width, int height, int color, ubyte alpha = 255) {
 
 	SDL_Rect rect = {x: x, y: y, w: width, h: height};
-
-	//save draw color
-	ubyte r,g,b,a;
-	SDL_GetRenderDrawColor(window.renderer, &r, &g, &b, &a);
-
-	SDL_SetRenderDrawColor(window.renderer, cast(ubyte)(color>>16), cast(ubyte)(color>>8), cast(ubyte)(color), alpha);
+	auto p = PushColor(window, cast(ubyte)(color>>16), cast(ubyte)(color>>8), cast(ubyte)(color), alpha);
 	(filled) ? SDL_RenderFillRect(window.renderer, &rect) : SDL_RenderDrawRect(window.renderer, &rect);
-
-	//reset draw color
-	SDL_SetRenderDrawColor(window.renderer, r, g, b, a);
 
 }
