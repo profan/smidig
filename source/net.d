0276243505065e40eea4b331ca796ba1ef7f8782
Robin Hübner <profan@pagefault.se>
2015-03-03 00:21:44 +0100

    Networking beginnings.

--- source/net.d
+++ source/net.d
@@ -1,5 +1,7 @@
 module sundownstandoff.net;
 
+import std.socket : InternetAddress, TcpSocket;
+
 enum MessageType {
 
 	CONNECT,
@@ -8,3 +10,26 @@ enum MessageType {
 	FIRE
 
 } //MessageType
+
+
+// Will live in a thread which the game sends and receives messages from!
+struct NetworkPeer {
+
+	bool open;
+	TcpSocket socket;
+
+	this(short port) {
+
+		this.socket = new TcpSocket();
+		this.socket.bind(new InternetAddress("localhost", 12000));
+
+	}
+
+	void listen() {
+		this.open = true;
+		while (open) {
+			socket.listen(1);
+		}
+	}
+
+} //NetworkPeer
