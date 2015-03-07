842d26d963438515339ed5ee818be4a7f12bb230
Robin Hübner <profan@pagefault.se>
2015-03-07 21:14:07 +0100

    GL code incoming.

---
+++ source/gl.d
@@ -0,0 +1,37 @@
+module sundownstandoff.gl;
+
+import std.c.stdlib;
+import core.stdc.stdio;
+import derelict.opengl3.gl3;
+
+//C-ish code ahoy
+bool check_shader_compile_success(GLuint shader) {
+
+	GLint length, result;
+
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE) {
+		char* log;
+		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &length);
+		log = cast(char*)malloc(length);
+		glGetShaderInfoLog(shader, length, &result, log);
+		printf("OpenGL Error: Unable to compile shader: %s", log);
+		free(log);
+		return false;
+	}
+
+	return true;
+
+}
+
+GLuint compile_shader(const(GLchar*)* shader_source, GLuint shader_type) {
+
+	GLuint new_shader;
+	
+	new_shader = glCreateShader(shader_type);
+	glShaderSource(new_shader, 1, shader_source, null);
+	glCompileShader(new_shader);
+
+	return new_shader;
+
+}
