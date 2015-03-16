dc0efa08c987a960703c492831ea6dbfdeaefd37
Robin Hübner <profan@pagefault.se>
2015-03-16 21:33:16 +0100

    Naive!

--- source/resource.d
+++ source/resource.d
@@ -1,3 +1,25 @@
 module sundownstandoff.resource;
 
 //some resource loading shenanigans, this will probably become some horrid singleton or global.
+
+class Resource {
+
+} //Resource
+
+class ResourceManager {
+
+	//some structure mapping an identifier to a texture
+
+	this() {
+
+	}
+
+	void load_texture(in char[] file_name) {
+		
+	}
+
+	auto get_texture(in char[] file_name) {
+		
+	}
+
+} //ResourceManager
