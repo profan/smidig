d8333cd975969a45ce3613e015bc5a6311ec98e4
Robin Hübner <profan@pagefault.se>
2015-03-17 22:22:16 +0100

    Player to unit, MAXIMUM PIVOT

--- source/ents.d
+++ source/ents.d
@@ -4,18 +4,18 @@ import profan.ecs;
 
 import sundownstandoff.sys;
 
-auto create_player(EntityManager em, Vec2f pos) {
+auto create_unit(EntityManager em, Vec2f pos) {
 
-	auto player = em.create_entity();
+	auto unit = em.create_entity();
 
 	MovementComponent mc = {velocity: Vec2f(0,0), position: pos};
-	em.register_component!MovementComponent(player, mc);
-	em.register_component!CollisionComponent(player); //beware of order, this depends on above component
-	auto cc = em.get_component!CollisionComponent(player);
+	em.register_component!MovementComponent(unit, mc);
+	em.register_component!CollisionComponent(unit); //beware of order, this depends on above component
+	auto cc = em.get_component!CollisionComponent(unit);
 	cc.radius = 32; //arbitrary number :D
-	em.register_component!InputComponent(player);
-	em.register_component!SpriteComponent(player);
+	em.register_component!InputComponent(unit);
+	em.register_component!SpriteComponent(unit);
 
-	return player;
+	return unit;
 
 }
