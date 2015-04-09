7104c8b72bd5b00dae1d3ce0652f51219814e28e
Robin Hübner <profan@pagefault.se>
2015-04-09 23:48:46 +0200

    Beginnings of some font atlas for text drawing.

--- source/engine/text.d
+++ source/engine/text.d
@@ -1,7 +1,10 @@
 module blindfire.text;
 
+import std.algorithm : max;
 import std.stdio : writefln;
+
 import derelict.freetype.ft;
+import derelict.opengl3.gl3;
 
 import blindfire.gl;
 
@@ -10,7 +13,6 @@ struct FontAtlas {
 	Texture atlas;
 
 	FT_Library ft;
-	FT_Face face;
 	FT_GlyphSlot g;
 
 	this(in char[] font_name, uint font_size) {
@@ -19,11 +21,26 @@ struct FontAtlas {
 			writefln("[GAME] Could not init freetype.");
 		}
 
+		FT_Face face;
 		if (FT_New_Face(ft, font_name.ptr, 0, &face)) { 
 			writefln("[GAME] Could not open font.");
 		}
 
 		FT_Set_Pixel_Sizes(face, 0, font_size);
+
+		int w, h;
+		for (uint i = 32; i < 128; ++i) {
+			if (FT_Load_Char(face, i, FT_LOAD_RENDER)) {
+				writefln("Character %c failed to load.", i);
+				continue;
+			}
+
+			w += g.bitmap.width;
+			h = max(h, g.bitmap.rows);
+
+		}
+
+		atlas = Texture(null, w, h, GL_ALPHA, GL_ALPHA);	
 
 	}
 
