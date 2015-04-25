f08dfd3b9d4a88f3a16c877390031b9e471f2ded
Robin Hübner <profan@pagefault.se>
2015-04-26 00:19:23 +0200

    Move common code to mixin.

--- source/engine/stream.d
+++ source/engine/stream.d
@@ -2,47 +2,7 @@ module blindfire.engine.stream;
 
 import std.traits : isArray;
 
-struct InputStream {
-
-	enum ReadMode {
-		Read,
-		Peek
-	}
-
-	private {
-		size_t size;
-		size_t offset = 0;
-		ubyte* buffer;
-	}
-
-	this(ubyte* data, size_t length) nothrow @nogc {
-		this.buffer = data;
-		this.size = length;
-	}
-
-	@property const(ubyte*) pointer() nothrow @nogc const {
-		return buffer + offset;
-	}
-
-	@property size_t current() const nothrow @nogc {
-		return offset;
-	}
-
-	@property size_t length() const nothrow @nogc {
-		return size;
-	}
-
-	T read(T, ReadMode mode = ReadMode.Read)() nothrow @nogc {
-		T obj = *(cast(T*)(buffer[offset..offset+T.sizeof].ptr));
-		static if (mode != ReadMode.Peek) {
-			offset += T.sizeof;
-		}
-		return obj;
-	}
-
-}
-
-struct OutputStream {
+mixin template StreamImpl() {
 
 	private {
 		size_t size;
@@ -70,6 +30,31 @@ struct OutputStream {
 	ubyte[] opSlice() nothrow @nogc {
 		return buffer[0..offset];
 	}
+
+}
+
+struct InputStream {
+
+	enum ReadMode {
+		Read,
+		Peek
+	}
+
+	mixin StreamImpl!();
+
+	T read(T, ReadMode mode = ReadMode.Read)() nothrow @nogc {
+		T obj = *(cast(T*)(buffer[offset..offset+T.sizeof].ptr));
+		static if (mode != ReadMode.Peek) {
+			offset += T.sizeof;
+		}
+		return obj;
+	}
+
+}
+
+struct OutputStream {
+
+	mixin StreamImpl!();
 
 	void write(T)(T obj) nothrow @nogc {
 		static if (isArray!(T)) {
