6264386a203cad8ec2ae09c682d0bf3bbcd49df5
Robin Hübner <profan@prfn.se>
2015-06-11 01:23:08 +0200

    Added some C-ish file read functions.

--- source/blindfire/engine/util.d
+++ source/blindfire/engine/util.d
@@ -22,3 +22,44 @@ void render_string(string format, Args...)(ref FontAtlas atlas, Window* window, 
 	offset.y += 16;
 
 }
+
+import core.stdc.stdlib : free, malloc;
+import core.stdc.stdio : rewind, fopen, fclose, fread, ftell, fseek, printf, FILE, SEEK_END;
+
+size_t get_filesize(FILE *file) nothrow @nogc {
+
+	fseek(file, 0, SEEK_END);
+	long size = ftell(file);
+ 	rewind(file);
+	if (size <= 0) printf("Invalid file size. \n");
+
+	return size;
+
+}
+
+size_t fread_str(char *buf, size_t buf_size, size_t filesize, FILE *file) nothrow @nogc {
+
+	size_t result = fread(buf, buf_size, filesize, file);
+	buf[filesize] = '\0';
+
+	return result;
+
+}
+
+char* load_file(const char *filename) nothrow @nogc {
+
+	FILE *file;
+	size_t result;
+	file = fopen(filename, "r");
+	if (file == null) printf("File error (does it exist?). \n");
+	size_t filesize = get_filesize(file);
+	
+	char *buf = cast(char*)malloc((char.sizeof*filesize)+1);
+	if (!buf) printf("Memory error. \n");
+	result = fread_str(buf, (*buf).sizeof, filesize, file);
+	if (result != filesize) printf("Reading error. \n");
+	fclose(file);
+
+	return buf;
+
+}
